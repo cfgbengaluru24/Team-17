1236132507f3a0c227457ce843768786a3072424
Anurag Guha <rick.anurag2@gmail.com>
2024-07-27 23:19:34 +0530

    Added backend integ to addPatients

--- Frontend/src/services/addPatient.jsx
+++ Frontend/src/services/addPatient.jsx
@@ -1,5 +1,9 @@
-const AddPatient = () => {
-  return <></>;
+import axios from "axios";
+const baseURL = "/api/addPatient";
+
+const addPatient = async (patientDetails) => {
+  const response = await axios.post(baseURL, patientDetails);
+  return response.data;
 };
 
-export default AddPatient;
+export default addPatient;
